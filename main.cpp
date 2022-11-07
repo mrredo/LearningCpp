862618de0c26a16e15635061a591db5c34a70a08
mrredo <74524695+mrredo@users.noreply.github.com>
2022-11-07 07:05:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
 
 #include "main.h"
 
-// string School::ReturnClassName() {
-//     return this->cName;
-// }
-// School::School(string clN) {
-// this->cName = clN;
-// }
+
 int main() {
     cout << "hello \n";
     School SCH("8B");
