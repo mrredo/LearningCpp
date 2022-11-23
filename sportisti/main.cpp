cedd2bd19c958f0b42a707be393a4dec9772d3cc
mrredo <mrredogt@gmail.com>
2022-11-23 21:23:10 +0200

    e

---
+++ sportisti/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int ten5 = 100000;
+struct sportists {
+    int nr;
+    int punkti;
+};
+int main() {
+    sportists Sportisti[12];
+    sportists sports;
+    
+
+}
+bool calcS(int s) {
+    return s <= ten5;
+}
